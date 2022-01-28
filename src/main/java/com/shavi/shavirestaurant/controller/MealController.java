b29e5148061f20dab6116900d8de1f1ada66522c
diaba <hwar@cincinnatistate.edu>
2022-01-28 13:24:31 -0500

    Add meal controller and service

--- src/main/java/com/shavi/shavirestaurant/controller/MealController.java
+++ src/main/java/com/shavi/shavirestaurant/controller/MealController.java
@@ -1,6 +1,8 @@
 package com.shavi.shavirestaurant.controller;
 import com.shavi.shavirestaurant.model.Category;
+import com.shavi.shavirestaurant.model.Meal;
 import com.shavi.shavirestaurant.service.CategoryService;
+import com.shavi.shavirestaurant.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,23 +11,21 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(path = "/api")
+@CrossOrigin
 public class MealController {
 
     private static final Logger LOGGER = Logger.getLogger(MealController.class.getName());
-    private CategoryService categoryService;
+    private MealService mealService;
 
     @Autowired
-    public void setCategoryService(CategoryService categoryService) {
-        this.categoryService = categoryService;
+    public void setMealService(MealService mealService) {
+        this.mealService = mealService;
     }
 
-    /**
-     *
-     * @return
-     */
-    @GetMapping("/categories/")
-    public List<Category> getCategories() {
-        return categoryService.getCategories();
+    @GetMapping(("/api/meals"))
+    public List<Meal> getAllMeals(){
+        return mealService.getAllMeals();
     }
+
 
 }
--- src/main/java/com/shavi/shavirestaurant/service/MealService.java
+++ src/main/java/com/shavi/shavirestaurant/service/MealService.java
@@ -1,13 +1,63 @@
 package com.shavi.shavirestaurant.service;
+import com.shavi.shavirestaurant.exception.InformationNotFoundException;
+import com.shavi.shavirestaurant.model.Meal;
+import com.shavi.shavirestaurant.repository.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Service
 public class MealService {
     private static final Logger LOGGER = Logger.getLogger(MealService.class.getName());
+
+    private MealRepository mealRepository;
+
+    @Autowired
+    public void setMealRepository(MealRepository mealRepository) {
+        this.mealRepository = mealRepository;
+    }
+
+    /**
+     * <p>Get all meals</p>
+     * @return
+     */
+    public List<Meal> getAllMeals(){
+        return mealRepository.findAll();
+    }
+
+    /**
+     * <p>Find a specific meal by id</p>
+     * @param mealId
+     * @return
+     */
+    public Meal getMeal(Long mealId){
+        Optional<Meal> meal = mealRepository.findById(mealId);
+        if (meal.isEmpty()){
+            throw new InformationNotFoundException("Meal is not found");
+        }
+        return meal.get();
+    }
+    /**
+     * <p>Find a specific meal by name</p>
+     * @param name
+     * @return
+     */
+    public Meal getMeal(String name){
+        Meal meal = mealRepository.findByName(name);
+        if (meal == null){
+            throw new InformationNotFoundException("Meal is not found");
+        }
+        return meal;
+    }
+    public Meal updateMeal(Long mealId){
+        Optional<Meal> meal = mealRepository.findById(mealId);
+        if (meal.isEmpty()){
+            throw new InformationNotFoundException("Meal is not found");
+        }
+        return mealRepository.save(meal.get());
+    }
 
 }
